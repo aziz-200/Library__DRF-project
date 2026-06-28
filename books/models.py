34a9b190e5174e50bca0c486aea4292e5f709d6b
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-06-28 15:53:29 +0500

    funtion views and RetrieveView are used:

--- books/models.py
+++ books/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
+    content = models.TextField()
     author = models.CharField(max_length=200)
     isbn = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import BookListApiView
+from .views import BookListApiView, book_list_view, BookDetailApiView, BookDeleteApiView, BookUpdateApiView
+
 urlpatterns = [
-    path('', BookListApiView.as_view())
+    path('', BookListApiView.as_view()),
+    path('<int:pk>', BookDetailApiView.as_view()),
+    path('books/', book_list_view),
+    path('<int:pk>/update/', BookUpdateApiView.as_view()),
+    path('<int:pk>/delete/', BookDeleteApiView.as_view()),
+
 ]
