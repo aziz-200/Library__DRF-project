5223abc0abb0dcb75587ab1e7a83667cd4084e11
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-06-28 18:33:54 +0500

    Swagger and redoc instalation:

--- library_project/urls.py
+++ library_project/urls.py
@@ -17,7 +17,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# SWAGGER settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library Project API",
+        default_version='v1',
+        description="Library Project API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="azizaxtamov0201@gmail.com"),
+        license=openapi.License(name='demo license')
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+#  swagger setting to here
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/v1/', include('books.urls')),
-]
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    ]
