d3c97f161cfbaaa8f4438df229de0a093d60c897
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-06-29 15:36:11 +0500

    Field validation and validation are written:

--- books/Serializers.py
+++ books/Serializers.py
@@ -9,6 +9,38 @@ class BookSerializer(serializers.ModelSerializer):
 
         fields = ('id', 'title', 'subtitle', 'content', 'author', 'isbn',  'price', )
 
+    # VALIDATION
+    def validate(self, data):
+
+        title = data.get('title', None)
+        author = data.get('author', None)
+
+
+
+        #  check title if  it contains only alphaber
+        if not title.isalpha():
+            raise serializers.ValidationError("Title must be alphanumeric")
+
+        # check title, and author from DB
+        if Book.objects.filter(title__iexact=title, author__iexact=author).exists():
+            raise serializers.ValidationError("Title already exists")
+
+        return data
+
+
+# FIELD VALIDATION => faqat fieldni validate qiladi
+    def validate_price(self, price):
+        if price <= 0 or price > 1000:
+            raise serializers.ValidationError("Price must be greater than 0")
+        return price
+
+
+
+
+
+
+
+
 
 # Serializer orqali yozish, manual writting => and without meta class ,
 # va modelga asoslanmagan holatlarda yozish tavsiya etiladi
